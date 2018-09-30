a55c06838263ac586611566a22fb4be8d31c0534
Patrick Maurits Sangian <patricksangian@gmail.com>
2018-09-30 20:09:46 +0700

    adding product routes | ep. 03

---
+++ api/routes/products.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /products'
+    })
+})
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discover the special ID',
+            id: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'You passed ad ID',
+            id: id
+        })
+    }
+})
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    res.status(200).json({
+        message: 'Updated product.',
+        id: id
+    })
+})
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    res.status(200).json({
+        message: 'Deleted product.',
+        id: id
+    })
+})
+
+
+module.exports = router
