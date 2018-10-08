195548054618e2436aff907f8d6373f942459a5e
Patrick Maurits Sangian <patricksangian@gmail.com>
2018-10-09 00:55:28 +0700

    refactor at products/id

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,84 +1,81 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
 // mongoose.Promise = require('bluebird')
 
-const Product = require('../models/product')
+const Product = require("../models/product");
 
-router.get('/', (req, res, next) => {
-    product = Product.find()
-    product.then(result => {
-        var products = []
-        result.forEach(function (item) {
-            var item = item.toObject()
-            item.access_link = {
-                uri: '/products/' + item._id,
-                method: 'GET'
-            }
-            products.push(item)
-        });
-        res.status(200).json({
-            product_list: products
-        })
-    })
-})
-
-router.post('/', (req, res, next) => {
-
-    var product = new Product({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        price: req.body.price
-    })
-
-    product.save()
-        .then(product => {
-            var result = product.toObject()
-            result.url = '/products/' + result._id
-            console.log(result)
-            res.status(201).json({
-                error: false,
-                message: 'Product created.',
-                crated_product: result
-            })
-        })
-        .catch(err => console.log(err))
-
-
-})
-
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId
-    const product = Product.find({
-        _id: id
-    }).exec()
-    product.then((doc) => {
-        res.status(200).json({
-            product: doc
-        })
-    }).catch((err) => {
-        res.status(404).json({
-            error: true,
-            message: err
-        })
-    })
-})
-
-router.patch('/:productId', (req, res, next) => {
-    const id = req.params.productId
+router.get("/", (req, res, next) => {
+  product = Product.find();
+  product.then(result => {
+    var products = [];
+    result.forEach(function(item) {
+      var item = item.toObject();
+      item.access_link = {
+        uri: "/products/" + item._id,
+        method: "GET"
+      };
+      products.push(item);
+    });
     res.status(200).json({
-        message: 'Updated product.',
-        id: id
+      product_list: products
+    });
+  });
+});
+
+router.post("/", (req, res, next) => {
+  var product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
+  });
+
+  product
+    .save()
+    .then(product => {
+      var result = product.toObject();
+      result.url = "/products/" + result._id;
+      console.log(result);
+      res.status(201).json({
+        error: false,
+        message: "Product created.",
+        crated_product: result
+      });
     })
-})
+    .catch(err => console.log(err));
+});
 
-router.delete('/:productId', (req, res, next) => {
-    const id = req.params.productId
-    res.status(200).json({
-        message: 'Deleted product.',
-        id: id
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      res.status(200).json({
+        product: doc
+      });
     })
-})
+    .catch(err => {
+      res.status(500).json({
+        error: true,
+        message: err
+      });
+    });
+});
 
+router.patch("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  res.status(200).json({
+    message: "Updated product.",
+    id: id
+  });
+});
 
-module.exports = router
+router.delete("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  res.status(200).json({
+    message: "Deleted product.",
+    id: id
+  });
+});
+
+module.exports = router;
