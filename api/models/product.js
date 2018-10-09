624becaa866aea5838657854fdf0179470357ae2
Patrick Maurits Sangian <patricksangian@gmail.com>
2018-10-09 13:19:41 +0700

    add version key on product schema

--- api/models/product.js
+++ api/models/product.js
@@ -4,6 +4,8 @@ const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: String,
     price: Number
+}, {
+    versionKey: '0.0.1 Beta'
 })
 
 module.exports = mongoose.model('Product', productSchema)
