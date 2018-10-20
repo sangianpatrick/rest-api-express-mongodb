d780fc63cfd659e686e662f27dff045318c6947c
Patrick Maurits Sangian <patricksangian@gmail.com>
2018-10-20 16:51:06 +0700

    rm cached

--- testCrypto.js
+++
@@ -1,6 +0,0 @@
-const {generateRandomString, hashString} = require('./helpers/cryptoHelper')
-
-const newSalt = '8e2ed6cac9'
-const salt = generateRandomString(10)
-const value = hashString('Passw0rd', newSalt)
-console.log(value)
